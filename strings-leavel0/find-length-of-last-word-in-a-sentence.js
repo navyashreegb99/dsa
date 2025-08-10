d1d6d2878b6bef29e6acbcfcb173c9e4d7c83253
Navyashree G B <navyashreegb99@gmail.com>
2025-08-10 12:32:59 +0530

    string-level0-leet-58: length of last word

--- strings-leavel0/find-length-of-last-word-in-a-sentence.js
+++ strings-leavel0/find-length-of-last-word-in-a-sentence.js
@@ -5,26 +5,11 @@
 var lengthOfLastWord = function (s) {
   /* Approach 1
     let splittedArray = s.trim().split(" ")
-    return splittedArray[splittedArray.length - 1].length; */
-
-  /*     // Approach2
-    //Trim extra spaces from end
-    let n = s.length - 1;
-    while (n >= 0) {
-        if (s[n] != ' ') break;
-        n--;
-    }
-
-    //n is at the position where word starts
-    //move till we find space at count the letters
-    let count = 0;
-    while (n >= 0) {
-        if (s[n] == ' ') break;
-        count++;
-        n--;
-    }
-    return count; */
-
+    return splittedArray[splittedArray.length - 1].length; 
+    T=O(n)
+    S=O(n)
+    */
+  /* 
   // Approach2
   //Trim extra spaces from end
   let n = s.length - 1;
@@ -42,4 +27,22 @@ var lengthOfLastWord = function (s) {
     n--;
   }
   return count;
+
+  TC=O(n)
+  SC=O(n) */
+
+  
+  /*
+  //Approach-3
+  let n = s.length - 1;
+  let count = 0;
+  while (n >= 0) {
+    if (s[n] !== " ") count++;
+    else if (count > 0) break;
+    n--;
+  }
+  return count;
+
+//   TC = O(n);
+//   SC = O(n); */
 };
