8fe37d2ddde73e5b713c6dec5bcb6e0335c67b19
Navyashree G B <navyashreegb99@gmail.com>
2025-08-18 23:42:16 +0530

    string-level-leet-125: Valid Palindrome using 2 pointer approach

---
+++ strings-leavel0/isPalindrome-two-pointer-approach.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  s = s.toLowerCase();
+  let left = 0;
+  let right = s.length - 1;
+  while (left <= right) {
+    if (!s[left].match(/[a-z0-9]/i)) {
+      left++;
+    } else if (!s[right].match(/[a-z0-9]/i)) {
+      right--;
+    } else {
+      if (s[left] !== s[right]) return false;
+      left++;
+      right--;
+    }
+  }
+  return true;
+};
+//TC=O(n)
+//SC=O(1)
