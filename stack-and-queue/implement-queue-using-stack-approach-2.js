2eb6600e0ea3d4b21ba409d3ba93235e01d981e6
Navyashree G B <navyashreegb99@gmail.com>
2025-08-31 22:32:46 +0530

    chore: update TC

--- stack-and-queue/implement-queue-using-stack-approach-2.js
+++ stack-and-queue/implement-queue-using-stack-approach-2.js
@@ -1,49 +1,52 @@
-
 var MyQueue = function () {
-    this.s1 = [];
-    this.s2 = [];
+  this.s1 = [];
+  this.s2 = [];
 };
 
-/** 
+/**
  * @param {number} x
  * @return {void}
  */
 MyQueue.prototype.push = function (x) {
-    this.s1.push(x);
+  this.s1.push(x);
 };
 
 /**
  * @return {number}
  */
+//TC: worst case - O(n);
+//TC: Average case - O(1)
 MyQueue.prototype.pop = function () {
-    if (this.s2.length === 0) {
-        while (this.s1.length) {
-            this.s2.push(this.s1.pop());
-        }
+  if (this.s2.length === 0) {
+    while (this.s1.length) {
+      this.s2.push(this.s1.pop());
     }
-    return this.s2.pop();
+  }
+  return this.s2.pop();
 };
 
 /**
  * @return {number}
  */
+//TC: worst case - O(n);
+//TC: Average case - O(1)
 MyQueue.prototype.peek = function () {
-    if (this.s2.length === 0) {
-        while (this.s1.length) {
-            this.s2.push(this.s1.pop());
-        }
+  if (this.s2.length === 0) {
+    while (this.s1.length) {
+      this.s2.push(this.s1.pop());
     }
-    return this.s2[this.s2.length - 1];
+  }
+  return this.s2[this.s2.length - 1];
 };
 
 /**
  * @return {boolean}
  */
 MyQueue.prototype.empty = function () {
-    return this.s1.length === 0 && this.s2.length === 0;
+  return this.s1.length === 0 && this.s2.length === 0;
 };
 
-/** 
+/**
  * Your MyQueue object will be instantiated and called as such:
  * var obj = new MyQueue()
  * obj.push(x)
