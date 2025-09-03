065656b10207e3094b232774db0b35ff5c188e20
Navyashree G B <navyashreegb99@gmail.com>
2025-09-03 23:06:41 +0530

    stack-leet-155: find min ele in stack in O(1) tc

---
+++ stack-and-queue/find-min-ele-stack-with-tc-O(1).js
@@ -0,0 +1,94 @@
+// var MinStack = function () {
+//   this.stack = [];
+// };
+
+// /**
+//  * @param {number} val
+//  * @return {void}
+//  */
+// MinStack.prototype.push = function (val) {
+//   if (this.stack.length === 0) this.stack.push(`${val},${val}`);
+//   else {
+//     let min = Math.min(
+//       val,
+//       Number(this.stack[this.stack.length - 1].split(",")[1])
+//     );
+//     this.stack.push(`${val},${min}`);
+//   }
+// };
+
+// /**
+//  * @return {void}
+//  */
+// MinStack.prototype.pop = function () {
+//   this.stack.pop();
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MinStack.prototype.top = function () {
+//   return Number(this.stack[this.stack.length - 1].split(",")[0]);
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MinStack.prototype.getMin = function () {
+//   return Number(this.stack[this.stack.length - 1].split(",")[1]);
+// };
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * var obj = new MinStack()
+//  * obj.push(val)
+//  * obj.pop()
+//  * var param_3 = obj.top()
+//  * var param_4 = obj.getMin()
+//  */
+
+var MinStack = function () {
+  this.stack = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  if (this.stack.length === 0) this.stack.push([val, val]);
+  else {
+    let min = Math.min(val, this.stack[this.stack.length - 1][1]);
+    this.stack.push([val, min]);
+  }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1][0];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.stack[this.stack.length - 1][1];
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
